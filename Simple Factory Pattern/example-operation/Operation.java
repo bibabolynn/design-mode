66ea88bd686a76b6199b9c5d450e5f263154f616
bibabolynn <1018527906@qq.com>
2018-03-31 10:55:11 +0800

    Update Operation.java

--- Simple Factory Pattern/example-operation/Operation.java
+++ Simple Factory Pattern/example-operation/Operation.java
@@ -1,19 +1,19 @@
-public abstract class Operation{   
-  private double numberA;
-  private double numberB;
-  public double getNumberA(){
-    return this.numberA;
-  }
-  public void setNumberA(double a){
-    this.numberA = a;
-  }
-  public double getNumberB(){
-    return this.numberB;
-  }
-  public void setNumberB(double b){
-    this.numberB = b;
-  }
-  
-  public abstract double getResult();
-  
+public abstract  class Operation {
+    private double numberA;
+    private double numberB;
+    public double getNumberA(){
+        return this.numberA;
+    }
+    public void setNumberA(double a){
+        this.numberA = a;
+    }
+    public double getNumberB(){
+        return this.numberB;
+    }
+    public void setNumberB(double b){
+        this.numberB = b;
+    }
+
+    public abstract double getResult() throws Exception;
+
 }
